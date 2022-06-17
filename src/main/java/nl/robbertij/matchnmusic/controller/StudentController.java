5bf1de1a225e00aef54ad40e20d8301d36861857
Robbery93 <89379889+Robbery93@users.noreply.github.com>
2022-06-17 12:30:02 +0200

    lag blijkbaar niet aan WebConfig...

--- src/main/java/nl/robbertij/matchnmusic/controller/StudentController.java
+++ src/main/java/nl/robbertij/matchnmusic/controller/StudentController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/students")
 public class StudentController {
     private final StudentService studentService;
